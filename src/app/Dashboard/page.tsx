5c82388dc2ba8c141c4de54e6a3ac7ae7f4d4835
Husnain Khan <hk292896@gmail.com>
2025-02-18 18:43:53 +0500

    2 pages

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,148 @@
+'use client'
+import React, { useState } from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { CiCircleChevRight } from 'react-icons/ci'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
+import { HiOutlineDotsVertical } from 'react-icons/hi'
+import { IoArrowUpCircleOutline, IoDocumentOutline, IoPersonOutline } from 'react-icons/io5'
+
+const page = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleContent = () => {
+        setIsOpen(!isOpen);
+    }
+    return (
+        <div>
+            <div className="bg-white w-full py-5">
+                <img src="/images/hsbc.png" className="lg:pl-32 pl-2 lg:h-10 w-auto" />
+            </div>
+            <div className="flex flex-col bg-[#3e515c] text-white w-full ">
+                <div className='py-5'>
+                    <div className='text-right px-2 lg:px-5 text-xs lg:text-medium'>
+                        Last logged on 18/02/2025 at 1:24:26 PM
+                    </div>
+                    <div className='px-2 lg:px-20'>
+                        <h1 className="text-3xl font-semibold pb-3">Good Evening</h1>
+                        <p>
+                            You have <span className="underline">2 new messages.</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='flex justify-end text-right px-2 lg:px-5'>
+                    <button
+                        className=' flex items-center text-white bg-[#374753] px-3 py-1 lg:px-7 lg:py-3 gap-2'
+                        onClick={toggleContent}
+                    >
+                        Quick Links
+                        {isOpen ? <FaAngleUp /> : <FaAngleDown />}
+                    </button>
+                </div>
+            </div>
+            <div>
+                {isOpen && (
+                    <div className='grid grid-cols-2 lg:grid-cols-7 bg-[#374753] py-8 text-white gap-5 text-center'>
+                        <div className='col-span-1 flex flex-col items-center px-14 '>
+                            <p className="pb-3">
+                                <IoArrowUpCircleOutline size={40} />
+                            </p>
+                            <p>Pay and Transfer</p>
+                        </div>
+                        <div className='col-span-1 flex flex-col  items-center px-14 '>
+                            <p className="pb-3">
+                                <CiCircleChevRight size={40} />
+                            </p>
+                            <p>Apply for a personal Loan</p>
+                        </div>
+                        <div className='col-span-1 flex flex-col  items-center px-14 '>
+                            <p className="pb-3">
+                                <IoDocumentOutline size={40} />
+                            </p>
+                            <p>eStatements</p>
+                        </div>
+                        <div className='col-span-1 flex flex-col items-center px-14 '>
+                            <p className="pb-3">
+                                <BsCurrencyDollar size={40} />
+                            </p>
+                            <p>Your investments</p>
+                        </div>
+                        <div className='col-span-1 flex flex-col items-center px-14'>
+                            <p className="pb-3">
+                                <IoPersonOutline size={40} />
+                            </p>
+                            <p>Apply for YourLife and YourCare</p>
+                        </div>
+                        <div className='col-span-1 flex flex-col items-center px-14'>
+                            <p className="pb-3">
+                                <IoArrowUpCircleOutline size={40} />
+                            </p>
+                            <p>Global Money Account</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className={`px-14 ${isOpen && '-mt-10'}`}>
+                <div className="mt-6 bg-white p-2 w-full overflow-x-auto sm:overflow-x-auto md:overflow-x-auto scroll-smooth px-5 shadow-lg rounded-sm">
+                    <div className="grid grid-cols-12 gap-4 text-gray-500 p-2 min-w-[1200px]">
+                        <div className='col-span-2'>
+                            <div className='lg:text-2xl font-semibold text-black'>CURRENT ACCOUNT</div>
+                            <div className='text-gray-500'>030-854053-001</div>
+                        </div>
+                        <div className='col-span-7'>
+                        </div>
+                        <div className='flex justify-end col-span-3'>
+                            <div className='flex flex-col text-end'>
+                                <div className='flex justify-end items-center gap-2'>
+                                    <p className='lg:text-xl font-semibold text-black'>331.71</p>
+                                    <p className='text-gray-500'>AED</p>
+                                </div>
+                                <div className='text-gray-500'>
+                                    Available Balance: 331.79 AED
+                                </div>
+                            </div>
+                            <div className="px-2 -mt-8 h-full border-r border-gray-300"></div>
+                            <div className='text-black'>
+                                <HiOutlineDotsVertical size={30} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 text-gray-500 p-2 min-w-[1200px]">
+                        <div className="col-span-2">13/02/2025</div>
+                        <div className="col-span-2 text-end">
+                            TRANSFER
+                        </div>
+                        <div className="col-span-6">
+                        </div>
+                        <div className=" flex justify-end col-span-2 px-10">
+                            -11,000.00
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 text-gray-500 p-2 min-w-[1200px]">
+                        <div className="col-span-2">12/02/2025</div>
+                        <div className="col-span-2 text-end">
+                            TRANSFER
+                        </div>
+                        <div className="col-span-6">
+                        </div>
+                        <div className=" flex justify-end col-span-2 px-10">
+                            5,383.37
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 text-gray-500 p-2 min-w-[1200px]">
+                        <div className="col-span-2">04/02/2025</div>
+                        <div className="col-span-2 text-end">
+                            TRANSFER
+                        </div>
+                        <div className="col-span-6">
+                        </div>
+                        <div className=" flex justify-end col-span-2 text-end px-10">
+                            3,393.50
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
